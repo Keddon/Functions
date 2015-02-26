8839d2643e94140f11d8065fd74877cc4101fe48
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 17:10:09 -0500

    Functions
    Invocation of the functions calcArea

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
  * Functions
  */
 
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+
+calcArea();
