40815876250265ce4e958b90e1517c195de8f42b
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 17:33:48 -0500

    Functions
    using the return to get values from functions.

--- js/script.js
+++ js/script.js
@@ -4,17 +4,26 @@
  */
 
 
-calcArea(30, 20);
+var total = calcArea(30, 20); //whatever the function calcArea returns , place it in 'total.
 
 function calcArea(w, h){
     var area = w * h;
     console.log(area);
+    return area; //return the value of 'area' to 'total'.
 }
+console.log(total);
 
-function dogYears(age){
-    var dogYears = age * 7
+
+
+
+
+
+
+function dogYears(age){ //'age' is the parameter. it stores the valse of the argument so that it can be used in the function
+    var dogYears = age * 7;
     console.log("Sparky is " + dogYears + " years old.")
 }
 
-dogYears(4);
+dogYears(4); //'4' is the argument being passed into the parameter 'age'
 dogYears(6);
+
