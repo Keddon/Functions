4b96b546b7bb327a8b79d152563ea74683d81cd5
Keddon Campbell <mrkfed101@gmail.com>
2015-02-26 17:26:21 -0500

    Functions
    Arguments and Parameter and how they are used to store data for the
    functions.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@
  * Functions
  */
 
-var width = 5;
 
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
+calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
     console.log(area);
 }
 
-console.log(width);
-calcArea();
-console.log(width);
+function dogYears(age){
+    var dogYears = age * 7
+    console.log("Sparky is " + dogYears + " years old.")
+}
+
+dogYears(4);
+dogYears(6);
